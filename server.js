a6845d3412be7a59973771b80795932ca8386988
jhonattan vargas <jhonattan.vargas@gmail.com>
2016-09-12 13:20:29 -0300

    septimo
    con aplicación que no es test

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ app.set('port', process.env.PORT || 3000);
 //app.set('port',3000);
 // Lo necesario para ocupar passport
 passport.use(new Strategy({
-    clientID: '1237889892938057',
-    clientSecret: '1c6b8c6b32c1cfda1c9969179e370e5f',
+    //clientID: '1237889892938057',
+    clientID: '1759592940975382',
+    //clientSecret: '1c6b8c6b32c1cfda1c9969179e370e5f',
+    clientSecret: '8e5d51d7ae1de27ed55b732826526b1b',
     //callbackURL: 'https://jhonattan-facebook-login-2.herokuapp.com:'+app.get('port')+'/login/facebook/return'
     callbackURL: 'https://jhonattan-facebook-login-2.herokuapp.com/login/facebook/return'
   },
